3785a6526f353acf71c193cc7d7134579de166d7
Herlina Pfeiffer <pfeiffer.herlina@gmx.net>
2023-06-06 14:40:53 +0200

    connecting with frontend

--- board/models.py
+++ board/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from datetime import date
 
-# Create your models here.
+
+class Todo(models.Model):
+    created_at = models.DateField(default=date.today)
+    title = models.CharField(max_length=500)
+    category = models.CharField(max_length=500)
+    description = models.CharField(max_length=500)
+    due_date = models.DateField(default=date.today)
+    urgency = models.CharField(max_length=500)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status = models.CharField(max_length=500)
--- board/views.py
+++ board/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from django.views import View
+from .models import Todo
+
+
+class TodoListView(View):
+    def get(self, request):
+        todos = Todo.objects.all()
+        allTasks = []
+        for todo in todos:
+            allTasks.append({
+                'created_at': todo.created_at,
+                'title': todo.title,
+                'category': todo.category,
+                'description': todo.description,
+                'due_date': todo.due_date,
+                'urgency': todo.urgency,
+                'assigned_user': todo.assigned_user.username,
+                'status': todo.status,
+            })
+        return JsonResponse(allTasks, safe=False)
